37ae82864869d6d5721891aed1d929ec06829a5f
Tomas Prerovsky <cepsdev@hotmail.com>
2022-06-28 22:05:23 +0200

    Step 3: Insertion Sort on numbers.

--- sorting.cpp
+++ sorting.cpp
@@ -3,5 +3,20 @@
 
 using namespace std;
 
-int main() {
+void insertion_sort(vector<int> & v){
+	for(ssize_t i = 1; i < v.size(); ++i){
+		auto key = v[i];
+		auto j = i - 1;
+		for(;j >= 0 && v[j] > key; --j)
+			v[j + 1] = v[j];
+		v[j + 1] = key;
+	}
+}
+
+int main(){
+    vector<int> v1 = {5,4,3,2,1};
+    insertion_sort(v1);
+    for( auto e: v1)
+        cout << e << ' ';
+    cout << '\n';
 }
